f21abd20719a3471a531ea93dd51c4eacac4528c
Abrshronaldo <abab61534@gmail.com>
2025-10-16 22:58:44 +0300

    d

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,7 +7,9 @@
 
 extern "C"
   JNIEXPORT jstring JNICALL
-Java_M_stringFromJNI(JNIEnv* env, jobject /* this */) {
+Java_M_stringFromJNI(JNIEnv* env, jobject ,jobject data) {
+
+
     return env->NewStringUTF("Hello from C++");
 }
                                                  
