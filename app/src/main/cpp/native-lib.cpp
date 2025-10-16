fa7d9a617901920baf2f53e5be7115d700d64f5d
Abrshronaldo <abab61534@gmail.com>
2025-10-16 23:19:25 +0300

    s

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,12 +4,12 @@
 
 
 #include <jni.h>
-
+#include<iostream>
 extern "C"
   JNIEXPORT jstring JNICALL
 Java_M_stringFromJNI(JNIEnv* env, jobject ,jobject data) {
 
-
+std::cout<<"ddd";
     return env->NewStringUTF("Hello from C++");
 }
                                                  
